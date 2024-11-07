33c90d0e03534f367f782903520370efceaaa0d4
Kurt Derrick <kdcalesa@gmail.com>
2024-11-07 09:48:11 +0400

    Coding is Cool

---
+++ Exercise 01/Coding Is Cool.py
@@ -0,0 +1,15 @@
+### Fill in the blanks below
+'''python
+word1 = 
+word2 = 
+word3 =
+
+# Use string concatenation to combine the variables and print the phrase
+_____(_________)
+
+'''
+
+word1 = "Coding"
+word2 = "Is"
+word3 = "Cool"
+print(word1 , word2 , word3 , end = "")
